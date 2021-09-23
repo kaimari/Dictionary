14592c24c5edd833ce73409fa8dd95fdb967074b
kaimari <90720400+kaimari@users.noreply.github.com>
2021-09-23 11:37:04 -0600

    Add files via upload

---
+++ Dictionarycode.py
@@ -0,0 +1,12 @@
+dict = {"Dessert":"Dessert is a sweet dish",
+        "Accept":"To accept means to recieve or agree to somthing",
+        "There":"There is an adverb of place which we user to talk about where somthing is.",
+        "Principle":"Principles are beliefs, values or basic truths",
+        "Advice":"Advice is a noun",
+        "Borrow":"To borrow means to receive something as a loan",
+        "Despite":"Despite is usually followed by a gerund",
+        "Although":"Although is usually followed by a whole clause.",
+        "Affect":"Affect is a very and effect is a noun.",
+        "Personal":"Your personal details including your name, age and nationality,"}
+key = input("type key for dict ")
+print(dict[key])
